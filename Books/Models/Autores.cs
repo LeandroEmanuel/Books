de6b615ce6ea92a4021e6714be2babcf5c29231a
Leandro Gomes <leandrog276@gmail.com>
2020-11-12 09:34:30 +0000

    criacao do modelo autores

---
+++ Books/Models/Autores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models
+{
+    public class Autores
+    {
+        public int AutorId { get; set; }
+        
+        [Required]
+        [StringLength(256)]
+        public String Nome { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
